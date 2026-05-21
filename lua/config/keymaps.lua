b1fe1d814a773b66d842947e3efb62fad67b5604
hannes3r <hannes.dreyer@oetker.com>
2026-05-21 16:27:21 +0200

    .

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+map("n", "<leader>ig", function()
+    require("sf").retrieve("-c")
+end, { desc = "Retrieve this file from org" })
+
+map("n", "<leader>ip", function()
+    require("sf").save_and_push("-c")
+end, { desc = "Deploy this file from org" })
+
+map("n", "<leader>id", function()
+    require("sf").diff_in_org()
+end, { desc = "Deploy this file from org" })
